df2e18ceb670bddb2af694467c7950436c103c9a
Nikita <nikita00890@gmail.com>
2019-11-05 20:23:49 +0200

    Main

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,37 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+
+import {IonicModule} from '@ionic/angular';
+
+import {TabsPage} from './tabs.page';
+
+const routes: Routes = [
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {path: 'main', loadChildren: '../main/main.module#MainPageModule'},
+            {path: 'calendar', loadChildren: '../calendar/calendar.module#CalendarPageModule'},
+            {path: 'options', loadChildren: '../options/options.module#OptionsPageModule'}
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/tabs/main',
+        pathMatch: 'full'
+    },
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [TabsPage]
+})
+export class TabsPageModule {
+}

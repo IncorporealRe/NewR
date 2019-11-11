43fa77780bd20d08544a158178f782dedeec25b7
Nikita <nikita00890@gmail.com>
2019-11-11 20:28:36 +0200

    Ionic Notifications WORK!!!!

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {AlertController, Platform} from '@ionic/angular';
+import {ELocalNotificationTriggerUnit, LocalNotifications} from '@ionic-native/local-notifications/ngx';
 
 @Component({
     selector: 'app-main',
@@ -7,12 +9,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-    constructor() {
+    constructor(private plt: Platform, private localNotifcations: LocalNotifications,
+                private alertCtrl: AlertController) {
+        this.plt.ready().then(() => {
+            this.localNotifcations.on('click').subscribe(res => {
+                let msg = res.data ? res.data.mydata : '';
+                this.showAlert(res.title, res.text, msg);
+            });
+
+            this.localNotifcations.on('trigger').subscribe(res => {
+
+            });
+        });
     }
 
     ngOnInit() {
     }
     Test() {
+this.localNotifcations.schedule({
+    id: 22,
+    title: 'Test',
+    text: 'Testing',
+    trigger: {every: ELocalNotificationTriggerUnit.MINUTE}
+});
+    }
+    showAlert(header, sub, msg) {
+        this.alertCtrl.create({
+            header: header,
+            subHeader: sub,
+            message: msg,
+            buttons: ['OK']
+        }).then(alert => alert.present);
 
     }
 
